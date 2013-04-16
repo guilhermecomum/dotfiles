f61bb7efe308117fe2b7f743fda23ec183eb730d
Guilherme Guerra <guerrinha@comum.org>
2013-04-16 15:08:15 -0300

    using flyspell mode on markdown files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -68,6 +68,8 @@
 
 ;; Markdown
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-hook 'markdown-mode-hook '(lambda() (flyspell-mode)))
 
 ;; Web Mode
 (add-to-list 'load-path "~/.emacs.d/elisp/web-mode")
