9db725a18fd60ff26463588113adc82f757f4a5e
Guilherme Guerra <guerrinha@Brubeck.home>
2014-10-20 17:13:57 -0200

    New emacs setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,6 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+(exec-path-from-shell-initialize)
 
 ;;------- Enviroment ---------
 
@@ -58,6 +59,21 @@
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 
+;; Flyspell
+(defun fd-switch-dictionary()
+  (interactive)
+  (let* ((dic ispell-current-dictionary)
+    	 (change (if (string= dic "pt_BR") "english" "pt_BR")))
+    (ispell-change-dictionary change)
+    (message "Dictionary switched from %s to %s" dic change)
+    ))
+
+(global-set-key (kbd "<f5>") 'fd-switch-dictionary)
+(global-set-key (kbd "<f6>") 'ispell-word)
+
+;; Disable Undo tree
+(setq undo-true-mode nil)
+
 ;;-------- Modes --------
 
 ;; Css
@@ -72,6 +88,7 @@
 ;; Markdown
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-hook 'markdown-mode-hook '(lambda() (flyspell-mode)))
 
 ;; Web Mode
 (require 'web-mode)
@@ -99,6 +116,11 @@
 ;; Slim-mode
 (require 'slim-mode)
 
+;; Javascript
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq-default js2-basic-offset 2)
+
 ;;-------- Interface --------
 
 ;; no bars
@@ -107,7 +129,8 @@
 (tool-bar-mode 0)
 
 ;; setting up a color theme
-(require 'monokai-theme)
+(load-theme 'monokai t)
+
 
 ;; show line numbers
 (require 'linum)
@@ -131,7 +154,15 @@
 ;; scroll smoothly
 (setq scroll-conservatively 10000)
 
+;; Nyan-mode
+(nyan-mode)
+
+;; Transparency
+(set-frame-parameter (selected-frame) 'alpha '(90 50))
+(add-to-list 'default-frame-alist '(alpha 90 50))
+
 ;;-------- Keybinds --------
+(require 'bind-key)
 
 ;; change window
 (global-set-key [C-tab] 'other-window)
@@ -145,10 +176,16 @@
 
 ;; buffer
 (global-set-key [A-tab] 'next-buffer)
-(global-set-key [S-A-iso-lefttab] 'previous-buffer)
+(global-set-key [S-A-tab] 'previous-buffer)
 
 ;; Navegation
-(global-set-key (kbd "s-g") 'goto-line)
+(global-set-key (kbd "M-g") 'goto-line)
+
+;; Sort
+(global-set-key (kbd "C-c s") 'sort-lines)
+
+;; Cool Enter
+(bind-key "C-j" 'newline-and-indent)
 
 ;; Multiple Cursors
 (global-set-key (kbd "C-c C-c") 'mc/edit-lines)
@@ -156,8 +193,90 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; highlight indentation column
+(global-set-key (kbd "M-1") 'highlight-indentation-current-column-mode)
+
+;; scrolling without changing the cursor
+(global-set-key [(meta n)] '(lambda () (interactive) (scroll-up 1)))
+(global-set-key [(meta p)] '(lambda () (interactive) (scroll-down 1)))
+
+;; scrolling other window
+(global-set-key [(meta j)] '(lambda () (interactive) (scroll-other-window 1)))
+(global-set-key [(meta k)] '(lambda () (interactive) (scroll-other-window -1)))
+
 ;;-------- Hooks ----------
 
 ;; Remove whitespace before save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(put 'downcase-region 'disabled nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(custom-safe-themes
+   (quote
+    ("bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" default)))
+ '(doc-view-continuous t)
+ '(fci-rule-color "#49483E")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#49483E" . 0)
+     ("#67930F" . 20)
+     ("#349B8D" . 30)
+     ("#21889B" . 50)
+     ("#968B26" . 60)
+     ("#A45E0A" . 70)
+     ("#A41F99" . 85)
+     ("#49483E" . 100))))
+ '(magit-diff-use-overlays nil)
+ '(syslog-debug-face
+   (quote
+    ((t :background unspecified :foreground "#A1EFE4" :weight bold))))
+ '(syslog-error-face
+   (quote
+    ((t :background unspecified :foreground "#F92672" :weight bold))))
+ '(syslog-hour-face (quote ((t :background unspecified :foreground "#A6E22E"))))
+ '(syslog-info-face
+   (quote
+    ((t :background unspecified :foreground "#66D9EF" :weight bold))))
+ '(syslog-ip-face (quote ((t :background unspecified :foreground "#E6DB74"))))
+ '(syslog-su-face (quote ((t :background unspecified :foreground "#FD5FF0"))))
+ '(syslog-warn-face
+   (quote
+    ((t :background unspecified :foreground "#FD971F" :weight bold))))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
